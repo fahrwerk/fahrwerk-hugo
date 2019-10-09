68c94c12112cfeccdcffc88492724ad82e08670e
Seryan Shana <seryan.shana@stud.h-da.de>
2019-10-09 14:09:17 +0200

    Änderungen neu

--- static/js/functions.js
+++ static/js/functions.js
@@ -2442,9 +2442,10 @@ var SEMICOLON = SEMICOLON || {};
 				if( $('body').hasClass('device-sm') || $('body').hasClass('device-xs') ) {
 
 					element.find('.tab-nav').addClass('d-none');
+					element.find('.tab-container').addClass('accordion '+ elementAccStyle +' clearfix').attr('data-active', tabActive);
 					element.find('.tab-content').addClass('acc_content');
 					element.find('.acctitle').removeClass('d-none');
-					SEMICOLON.widget.accordions();
+					element.tabs('refresh');
 
 				} else if( $('body').hasClass('device-md') || $('body').hasClass('device-lg') || $('body').hasClass('device-xl') ) {
 
